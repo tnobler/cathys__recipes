9891d920a497ddf4ad61d4927d9dbd9e8af608d4
Tanner Noble <tannernoble@gmail.com>
2019-12-19 16:23:18 -0600

    added render method to NewRecipe component and added route to Index.jsx

--- app/javascript/components/NewRecipe.jsx
+++ app/javascript/components/NewRecipe.jsx
@@ -3,8 +3,7 @@ import { Link } from "react-router-dom";
 
 class NewRecipe extends React.Component {
   constructor(props) {
-    super(props)
-    
+    super(props);
     this.state = {
       name: "",
       ingredients: "",
@@ -13,7 +12,7 @@ class NewRecipe extends React.Component {
 
     this.onChange = this.onChange.bind(this);
     this.onSubmit = this.onSubmit.bind(this);
-    this.stripHtmlEntities = this.stripHtmlEntities.bind(this);    
+    this.stripHtmlEntities = this.stripHtmlEntities.bind(this);
   }
 
   stripHtmlEntities(str) {
@@ -40,7 +39,7 @@ class NewRecipe extends React.Component {
       instruction: instruction.replace(/\n/g, "<br> <br>")
     };
 
-    const token = document.querySelector('meta[name="csrf-token"]').textContent;
+    const token = document.querySelector('meta[name="csrf-token"]').content;
     fetch(url, {
       method: "POST",
       headers: {
@@ -58,6 +57,63 @@ class NewRecipe extends React.Component {
       .then(response => this.props.history.push(`/recipe/${response.id}`))
       .catch(error => console.log(error.message));
   }
+
+  render() {
+    return (
+      <div className="container mt-5">
+        <div className="row">
+          <div className="col-sm-12 col-lg-6 offset-lg-3">
+            <h1 className="font-weight-normal mb-5">
+              Add a new recipe to our awesome recipe collection.
+            </h1>
+            <form onSubmit={this.onSubmit}>
+              <div className="form-group">
+                <label htmlFor="recipeName">Recipe name</label>
+                <input
+                  type="text"
+                  name="name"
+                  id="recipeName"
+                  className="form-control"
+                  required
+                  onChange={this.onChange}
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="recipeIngredients">Ingredients</label>
+                <input
+                  type="text"
+                  name="ingredients"
+                  id="recipeIngredients"
+                  className="form-control"
+                  required
+                  onChange={this.onChange}
+                />
+                <small id="ingredientsHelp" className="form-text text-muted">
+                  Separate each ingredient with a comma.
+                </small>
+              </div>
+              <label htmlFor="instruction">Preparation Instructions</label>
+              <textarea
+                className="form-control"
+                id="instruction"
+                name="instruction"
+                rows="5"
+                required
+                onChange={this.onChange}
+              />
+              <button type="submit" className="btn custom-button mt-3">
+                Create Recipe
+              </button>
+              <Link to="/recipes" className="btn btn-link mt-3">
+                Back to recipes
+              </Link>
+            </form>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
 }
 
 export default NewRecipe;
