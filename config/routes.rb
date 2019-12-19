1754b625b56b384728b4365167ad197d0488f977
Tanner Noble <tannernoble@gmail.com>
2019-12-19 15:51:11 -0600

    corrected show route in routes.rb, created Recipe component and logic to show individual recipe

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       get 'recipes/index'
       post 'recipes/create'
-      get '/show/:id', to: 'resipes#show'
+      get '/show/:id', to: 'recipes#show'
       delete '/destroy/:id', to: 'recipes#destroy'
     end
   end
